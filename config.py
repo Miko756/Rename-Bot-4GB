766271538fb9a1fba3872b2c598d083038e91939
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-06-18 23:30:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6520062212:AAHRbNShxzyG_bsn3NDyaiKR2eK1FXxEIZ4")
+API_ID = int(os.environ.get("API_ID", "29759992"))
+API_HASH = os.environ.get("API_HASH", "61f150cdca64b2916fa499d107393140")
+OWNER = int(os.environ.get("OWNER", "5787502520"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Fubukiera756_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002019186374")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001922369042"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://ZeroTwo:aloksingh@zerotwo2.201lbx7.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+STRING = os.environ.get("STRING", "BQHGGfgAaPpbKpvTg5bo69SyKhgUFNJan1c1fV7SMpWnX16qyouFpGZg6GG6hllIpPqevAT16TvVYQwW1w8SMhM1iBxyt86DmjowC2EPYl_mObnvtBBvVURLXgwA-SMBNoGokw0eWvcrJczuqe6sehUrbCaLhp_fNoQhWs85CAXwfdexQtUwFN6JduTUvKWLOnHTtMhunLznSTdpRpYAlcuQdI4e44nRawUOszugD9bkanJ-vekctRbZgQLGuwpZc9s5Apu_7D6Tpza333d3dqvRY8BLuM8BRy25hoxWI0kLbKH_d6woYPU3RZ83Dtc0INy3l3aP2wIxy4m7rErZ-7aOsIqs3wAAAAFY9ke4AA")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/114432cb06bd656ee765b.jpg")
 
 
 
